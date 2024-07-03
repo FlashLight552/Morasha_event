11af7305cba97121fa692d5dbd138240a163c95a
FlashLight552 <91726561+FlashLight552@users.noreply.github.com>
2024-07-03 13:52:55 +0300

    upd

--- morasha/morasha/settings.py
+++ morasha/morasha/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     "event.apps.EventConfig",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,23 +77,23 @@ WSGI_APPLICATION = 'morasha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#     "ENGINE": "django.db.backends.mysql",
-#     "NAME": "morasha",
-#     "USER":"morasha",
-#     "PASSWORD":"morasha",
-#     "HOST":"mariabd",
-#     "PORT":"3306",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": "morasha",
+    "USER":"morasha",
+    "PASSWORD":"morasha",
+    "HOST":"mariabd",
+    "PORT":"3306",
+    }
+}
 
 
 # Password validation
@@ -129,8 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'event/static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'event/staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'event/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'event/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
